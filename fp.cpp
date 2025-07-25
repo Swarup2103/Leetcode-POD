909fea2a2afb7a6e610e65e634450deef0a285fd
Swarup Biraje <swaruprb2103@gmail.com>
2025-07-25 22:36:12 +0530

    first commit

---
+++ fp.cpp
@@ -0,0 +1,26 @@
+//1 March, 2025
+
+//2460. Apply Operations to an Array
+
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& nums) {
+        int size = nums.size();
+        for(int i = 0; i < size-1; i++) {
+            if(nums[i]==nums[i+1]) {
+                nums[i] = nums[i]*2;
+                nums[i+1] = 0;
+            }
+        }
+        int posOfZero = 0; 
+
+        for (int i = 0; i < size; i++) {
+            if (nums[i] != 0) {
+                swap(nums[i], nums[posOfZero]); 
+                posOfZero++;
+            }
+        }
+        return nums;
+    }
+        
+};
